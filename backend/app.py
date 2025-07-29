816fbbc13f735d96b45434acd0e77b9d143d6ad5
Hala-Alluhaidan <Alluhaidan7ala@gmail.com>
2025-07-29 23:46:38 +0300

    Connect /log endpoint to write real-time phishing data to logs.csv

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import csv
 import os
 
 app = Flask(__name__)
+CORS(app)
 LOG_PATH = "backend/logs.csv"
 
 @app.route('/log', methods=['POST'])
